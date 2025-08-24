0defa7346d750e2efec2debbdc3eb5feb582d4c3
sabov241991-glitch <sabov.24.1991@gmail.com>
2025-08-24 13:02:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
+ADMIN_IDS = [123456789]
+DATABASE_PATH = 'davinchik.db'
+
+ROLES = {
+    'user': 0,
+    'moder': 1,
+    'admin': 2,
+    'head_admin': 3
+}
+
+# Настройки авто-повышения
+AUTO_PROMOTION = {
+    'moder': {'messages': 1000, 'violations': 0},
+    'admin': {'messages': 10000, 'violations': 0}
+}
+
+# Звездочки за сообщения
+STAR_LEVELS = {
+    500: '⭐',
+    1000: '⭐⭐',
+    5000: '⭐⭐⭐',
+    10000: '⭐⭐⭐⭐',
+    20000: '⭐⭐⭐⭐⭐'
+}
+
+# Фильтр рекламы
+BLACKLIST_DOMAINS = ['.ru', '.com', '.net', '.org', 't.me', '@', 'instagram', 'vk.com']
