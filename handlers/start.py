64c256fd80f32f0226a1b3d8e0d3e364516f69fe
sabov241991-glitch <sabov.24.1991@gmail.com>
2025-08-24 14:55:21 +0300

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,6 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await update.message.reply_text(f"Привет, {user.first_name}! 🎉")
