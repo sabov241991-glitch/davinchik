8960b57d5e4e3465f2fe3df0289b20f0271c957e
sabov241991-glitch <sabov.24.1991@gmail.com>
2025-08-24 16:07:07 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+BOT_TOKEN = "ТВОЙ_ТОКЕН"
+ADMIN_IDS = [1718901610]  # Твой ID
+
+# Настройки
+ROLES = {
+    'user': 0,
+    'moder': 1, 
+    'admin': 2,
+    'head_admin': 3
+}
+
+AUTO_PROMOTION = {
+    'moder': {'messages': 1000, 'violations': 0},
+    'admin': {'messages': 10000, 'violations': 0}
+}
+
+STAR_LEVELS = {
+    500: '⭐',
+    1000: '⭐⭐', 
+    5000: '⭐⭐⭐',
+    10000: '⭐⭐⭐⭐',
+    20000: '⭐⭐⭐⭐⭐'
+}
